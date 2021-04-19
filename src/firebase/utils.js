843af3f51b0a87071e2e6e6430fe0eb8d2f360fb
meshrafa <kokiazzam19@gmail.com>
2021-04-19 16:33:24 +0200

    fix-conflict

--- src/firebase/utils.js
+++ src/firebase/utils.js
@@ -5,7 +5,6 @@ import { firebaseConfig } from "./config";
 
 firebase.initializeApp(firebaseConfig);
 
-<<<<<<< HEAD
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 export const GoogleProvider = new firebase.auth.GoogleAuthProvider();
@@ -39,46 +38,6 @@ export const handleUserProfile = async (userAuth, usertype, additionalData) => {
       });
     } catch (err) {
       console.log(err);
-=======
-firebase.initializeApp(firebaseConfig)
-
-
-
-export const  auth = firebase.auth();
-export const db = firebase.firestore();
-export const GoogleProvider = new firebase.auth.GoogleAuthProvider();
-  GoogleProvider.setCustomParameters({prompt:'select_account'});
-
-
-export const handleUserProfile = async (userAuth,additionalData)=>{
-    //userAuth/additionalData object from google
-    if(!userAuth) return ;
-    const {uid} = userAuth;
-    //firebase request
-    const userRef = db.doc(`users/${uid}`);
-    // fetch/read the data from firebase
-    const snapshot = await userRef.get();
-    if(!snapshot.exists){
-        // else doesn't exist  so create a new user
-        const {displayName,email,photoURL} = userAuth;
-        const timesTamp = new Date();
-        //const userRoles = ['user'];
-        const userRoles = (email ==='mera.saber@yahoo.com')?['user','admin']:'user';
-        try{
-            await userRef.set({
-                displayName,
-                email,
-                photoURL,
-                createDate : timesTamp,
-                userRoles,
-                ...additionalData
-            })
-
-        } catch(err){
-        console.log(err)
-      }
-  
->>>>>>> 9295ce38fd9752d42766cd38eb2ab788991649fc
     }
   }
   return userRef;
